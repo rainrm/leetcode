d9a642071991d9422e374b444ea13a515eb17195
JiYanli <1173245767@qq.com>
2022-05-19 12:09:25 +0800

    third commit

--- src/main/java/com/rainrm/test/TestGit.java
+++ src/main/java/com/rainrm/test/TestGit.java
@@ -3,5 +3,6 @@ package com.rainrm.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
 }
